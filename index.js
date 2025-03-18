db4ff2f86a8d42160495714805ce5641caebcc86
Mr. Azharuddin <azharquraishi94@gmail.com>
2025-03-18 13:41:21 +0530

    Ok

--- index.js
+++ index.js
@@ -19,8 +19,9 @@ const uri = process.env.MONGO_URL;
 
 const app = express();
 
-app.use(cors({ origin: "https://z-fnd.netlify.app/"}));
+app.use(cors({ origin: "*", methods: "GET,POST,PUT,DELETE"}));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 app.use(cookieParser());
